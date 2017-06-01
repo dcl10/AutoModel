2216f9b7b292f06e6ed4ced89781da42f20a940b
dcl10 <lea.daniel@hotmail.co.uk>
2017-06-01 12:20:47 +0100

    Add a little user interactivity.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,38 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../terminal.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("terminal.fxml"));
         primaryStage.setTitle("AutoModel");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(event -> {
+            event.consume();
+            exit();
+        });
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void exit() {
+        int close = JOptionPane.showConfirmDialog(null, "Close AutoModel?", "Close",
+                JOptionPane.YES_NO_OPTION);
+        if (close == JOptionPane.YES_OPTION){
+            Platform.exit();
+        }
     }
 }

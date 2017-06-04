7778f6b16f3e95c4536fedef9b9121043c1c212f
dcl10 <lea.daniel@hotmail.co.uk>
2017-06-04 16:51:10 +0100

    Wrote a Perl script to parse BLAST search results and retrieve the PDB file of the best hit.

---
+++ additional/result_parse.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+# Module required to parse BLAST search results.
+use Bio::SearchIO;
+
+# Capture file to be parsed from @ARGV array.
+my $input = $ARGV[0];
+# Variable to hold the ID of the top hit.
+my $seqid;
+
+# Factory for the parser. Set the format to BLAST and file to be
+# parsed as the file passed to the script in @ARGV.
+my $searchio = Bio::SearchIO -> new(
+    -format => 'blast',
+    -file => "$input"
+);
+
+# Retrieve the ID of the top hit.
+while (my $result = $searchio -> next_result) {
+    if ($result -> num_hits == 0) {print "No results found.\n";}
+    else {
+        while (my $hit = $result -> next_hit) {
+            $seqid = $hit -> name;
+            last;
+        }
+    }
+}
+
+# Clip the chain identifier from the ID.
+$seqid =~ s/_.?/.pdb/;
+
+# Retrieve the appropriate PDB file from the PDB database.
+system "wget https://files.rcsb.org/download/$seqid";
